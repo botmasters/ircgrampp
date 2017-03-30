61b1bd12cef7a1b46dbfd1e8f0a8c54e5019ea92
exos <oscar@gentisoft.com>
2017-03-29 23:31:37 -0300

    Add cli module and bin

---
+++ src/cli/index.js
@@ -0,0 +1,46 @@
+
+import program from "commander";
+import packageInfo from "../../package.json";
+
+/*process.on("uncaughtException", (err) => {
+    process.stderr.write(`${err}\n`);
+    process.exit(12);
+});*/
+
+program
+    .version(packageInfo.version)
+    .description(packageInfo.description)
+    .option("--debug [modules]", "Debug mode");
+
+program
+    .command("start")
+    .alias("s")
+    .description("Start bridges")
+    .option("-d, --daemon", "Start in background")
+    .option("--only <bridge>", "Start only the specific bridge")
+    .action(() => {
+    
+    });
+
+program
+    .command('*')
+    .action(() => {
+        process.stderr.write(`The command ${program.args[0]}, is not a valid` +
+                             ` command, see ${program._name} --help\n`);
+        process.exit(1);
+    });
+
+const mainCLI = function (args) {
+    program.parse(args);
+
+    if (program.debug) {
+        process.env.DEBUG=program.debug;
+    }
+
+    if (!program.args.length) {
+        program.help();
+    }
+
+};
+
+export default mainCLI;
