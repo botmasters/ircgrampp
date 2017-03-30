f5eab78b6231640bdb5542fcbf7e88daf01009f7
exos <tioscar@gmail.com>
2017-03-30 18:20:10 -0300

    Prepare

---
+++ src/index.js
@@ -0,0 +1,18 @@
+
+import * as bridge from "./bridge";
+import * as config from "./config";
+import * as irc from "./irc";
+import * as session from "./session";
+import * as storage from "./storage";
+import * as telegram from "./telegram";
+import * as cli from "./cli";
+
+export {
+    bridge,
+    config,
+    irc,
+    session,
+    storage,
+    telegram,
+    cli
+};
