8d0afda80cedc21152e78b675702a79c75c310cb
exos <oscar@gentisoft.com>
2017-03-31 00:45:08 -0300

    Add hook (base functions and class)

---
+++ src/hooks.js
@@ -0,0 +1,58 @@
+
+import debugLib from "debug";
+
+var Promise = require("bluebird");
+
+let hooks = [];
+
+export class Hook {
+
+    constructor(name) {
+        Hook.validateHookName(name);
+        this._name = name;
+        this._debug = debugLib(`hooks ${name}`);
+        hooks.push(this);
+    }
+
+    before(data) {
+        this._debug(`hit before`);
+        return Promise.resolve(data);
+    }
+
+    beforeSync(data) {
+        this._debug(`hit before`);
+        return data;
+    }
+
+    after(data) {
+        this._debug(`hit after`);
+        return Promise.resolve(data);
+    }
+
+    afterSync(data) {
+        this._debug(`hit after`);
+        return data;
+    }
+
+    get name() {
+        return this._name; 
+    }
+
+    static validateHookName(name) {
+        let actual = hooks.find(x => x.name === name);
+
+        if (actual) {
+            throw new Error("Hook already exists");
+        }
+
+        if (!name.match(/^[a-z]+:[a-z]+(\.[a-z]+)*$/i)) {
+            throw new Error("Invalid hook name");
+        } 
+
+    }
+
+}
+
+export const declareHook = function (name) {
+    return new Hook(name);
+}
