9f8b4e3cd4b19fd09081765da9ceeb0974e3b474
exos <oscar@gentisoft.com>
2017-03-31 00:45:27 -0300

    Implement hooks in Session

--- src/session.js
+++ src/session.js
@@ -3,6 +3,7 @@ import {EventEmitter} from "events";
 import debugLib from "debug";
 import Bridge from "./bridge";
 import config, {getBridgeConfig} from "./config"; 
+import {declareHook} from "./hooks";
 
 var Promise = require("bluebird");
 
@@ -23,6 +24,14 @@ export default class Session extends EventEmitter {
         this._bridgesConfig = this._makeBridgesTree();
         this._started = false;
         this._bridges = [];
+
+        this._hooks = {
+            create: declareHook("session:create"),
+            start: declareHook("session:start"),
+            startBridge: declareHook("session:brige.start"),
+        }
+
+        this._hooks.create.after(this);
     }
 
     _makeBridgesTree() {
@@ -44,6 +53,47 @@ export default class Session extends EventEmitter {
             .filter(n => n.enable);
     }
 
+    startBridge(oircChannel, otelegramChannel, obridgeConfig) {
+
+        let bconfig = this._hooks.startBridge.beforeSync({ 
+            ircChannel: oircChannel,
+            telegramChannel: otelegramChannel,
+            bridgeConfig: obridgeConfig
+        });
+
+        let {ircChannel, telegramChannel, bridgeConfig} = bconfig;
+
+        let bridge = new Bridge(
+            bridgeConfig.name,
+            ircChannel,
+            telegramChannel,
+            bridgeConfig
+        );
+
+        Promise.all([
+            new Promise((resolve) => {
+                bridge.once("irc:registered", () => {
+                    debug(
+                        `Bridge ${bridgeConfig.name} registered with IRC`);
+                    return resolve();
+                });
+            }),
+            new Promise((resolve) => {
+                bridge.once("telegram:me", () => {
+                    debug(
+                        `Bridge ${bridgeConfig.name} success with Telegram`);
+                    return resolve();
+                });
+            })
+        ]).then(() => {
+            debug(`Bridge ${bridgeConfig.name} stablished`);
+            this.emit("bridgestablished", bridgeConfig.name);
+        });
+
+        return this._hooks.startBridge.afterSync(bridge);
+
+    }
+
     start() {
 
         if (this._started) {
@@ -55,51 +105,31 @@ export default class Session extends EventEmitter {
         }
 
         debug("Starting bridges");
+        this._started = true;
 
-        this._bridges = this._bridgesConfig.map((bridgeConfig) => {
-            let ircChannel = bridgeConfig.irc.channel;
-            let telegramChannel = bridgeConfig.telegram.channel;
+        this._hooks.start.before(this._bridgesConfig).then((config) => {
 
-            if (!ircChannel || !telegramChannel) {
-                throw new Error(`Bridge ${bridgeConfig.name} has a error in ` +
-                                `IRC or telegram channel definition`);
-            }
+            this._bridges = config.map((bridgeConfig) => {
+                let ircChannel = bridgeConfig.irc.channel;
+                let telegramChannel = bridgeConfig.telegram.channel;
 
-            debug(`Starting ${bridgeConfig.name} ${ircChannel} <-> ` + 
-                  ` ${telegramChannel}`);
+                if (!ircChannel || !telegramChannel) {
+                    throw new Error(`Bridge ${bridgeConfig.name} has a ` +
+                        `error in IRC or telegram channel definition`);
+                }
 
-            let bridge = new Bridge(
-                bridgeConfig.name,
-                ircChannel,
-                telegramChannel,
-                bridgeConfig
-            );
+                debug(`Starting ${bridgeConfig.name} ${ircChannel} <-> ` + 
+                      ` ${telegramChannel}`);
 
-            Promise.all([
-                new Promise((resolve) => {
-                    bridge.once("irc:registered", () => {
-                        debug(
-                            `Bridge ${bridgeConfig.name} registered with IRC`);
-                        return resolve();
-                    });
-                }),
-                new Promise((resolve) => {
-                    bridge.once("telegram:me", () => {
-                        debug(
-                          `Bridge ${bridgeConfig.name} success with Telegram`);
-                        return resolve();
-                    });
-                })
-            ]).then(() => {
-                debug(`Bridge ${bridgeConfig.name} stablished`);
-                this.emit("bridgestablished", bridgeConfig.name);
+                return this.startBridge(
+                    ircChannel,
+                    telegramChannel,
+                    bridgeConfig); 
+   
             });
 
-            return bridge;
-
+            this._hooks.start.after(this._bridges);
         });
-
-        this._started = true;
 
     }
 
